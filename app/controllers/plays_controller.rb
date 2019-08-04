75b4ca926a33664e5107ba4f523fa7296b2b8dd3
Alvin <alvinchun911@gmail.com>
2019-08-04 00:12:12 -0400

    Assigned Category to Plays

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -5,14 +5,18 @@ class PlaysController < ApplicationController
 		@plays = Play.all.order('created_at DESC')
 	end
 
-	def show; end
+	def show
+		@play
+	end
 
 	def new
-		@play = Play.new
+		@play = current_user.plays.build
+		@categories = Category.all.map { |c| [c.name, c.id] }
 	end
 
 	def create
-		@play = Play.new(play_params)
+		@play = current_user.plays.build(play_params)
+		@play.category_id = params[:category_id]
 
 		if @play.save
 			redirect_to root_path
@@ -21,9 +25,13 @@ class PlaysController < ApplicationController
 		end
 	end
 
-	def edit; end
+	def edit
+		@categories = Category.all.map { |c| [c.name, c.id] }
+	end
 
 	def update
+		@play.category_id = params[:category_id]
+
 		if @play.update(play_params)
 			redirect_to play_path(@play)
 		else
@@ -39,7 +47,7 @@ class PlaysController < ApplicationController
 	private
 
 	def play_params
-		params.require(:play).permit(:title, :description, :director)
+		params.require(:play).permit(:title, :description, :director, :category_id)
 	end
 
 	def find_play
